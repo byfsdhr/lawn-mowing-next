8fe1f19e82ffd9fb24f3afb46548586b1feac2ee
Yifan Bao <bbbbb8bbbbb@gmail.com>
2021-08-23 00:34:28 +1200

    created navbar and logo

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,10 @@
-// import Navbar from './navbar'
+import Navbar from "./Navbar"
 // import Footer from './footer'
 
 export default function Layout({ children }) {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <main>{children}</main>
       {/* <Footer /> */}
     </>
