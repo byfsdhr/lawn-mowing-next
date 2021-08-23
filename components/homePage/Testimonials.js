fdd9270bb5a22d1eff45ecededfb93da8cd01aff
Yifan Bao <bbbbb8bbbbb@gmail.com>
2021-08-23 13:55:53 +1200

    done simple testimonials

---
+++ components/homePage/Testimonials.js
@@ -0,0 +1,45 @@
+export default function Testimonial() {
+  const testimonials = [
+    {
+      id:1,
+      name: "John Doe",
+      say: "Monitoring and tracking my health vitals anywhere I go and on any platform I use has never been easier.",
+    },
+    {
+      id:2,
+      name: "Trumph Doe",
+      say: "As an Athlete, this is the perfect product for me. I wear my Smart Health Monitoring Wristwatch everywhere I go, even in the bathroom since it's waterproof.",
+    },
+    {
+      id:3,
+      name: "Yeo Ha",
+      say: " I don't regret buying this wearble gadget. One of the best gadgets I own!.",
+    },
+  ];
+
+  return (
+    <div className="bg-gray-200">
+      <div className="container mx-auto px-10 py-20">
+        <h2 className="text-4xl font-bold text-center text-gray-800 mb-8">
+          Testimonials
+        </h2>
+        <div className="flex flex-wrap">
+          {testimonials.map((t) => {
+            return (
+              <div key={t.id} className="w-full md:w-1/3 px-2 mb-4">
+                <div className="bg-white rounded shadow py-2">
+                  <p className="text-gray-800 text-base px-6 mb-5">
+                    {t.say}
+                  </p>
+                  <p className="text-gray-500 text-xs md:text-sm px-6">
+                    {t.name}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Hero from "../components/homePage/Hero";
+import Testimonial from "../components/homePage/Testimonials";
 // TODO create home page(pretty)
 export default function Home() {
   return (
     <div>
-      <Hero ></Hero>
+      <Hero />
+      <Testimonial / >
     </div>
   );
 }
